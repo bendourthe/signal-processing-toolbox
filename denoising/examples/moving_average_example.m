deee64318107ec7f6988ef9a79ea10a2d4a3049f
BenD <benjamin.dourthe@gmail.com>
2019-05-13 20:29:09 -0400

    moving_average: created Matlab function and example code

---
+++ denoising/examples/moving_average_example.m
@@ -0,0 +1,20 @@
+% Generate a random signal
+sampling_rate = 1000; % Hz
+time = 0:1/sampling_rate:3;
+n = length(time);
+p = 15; % poles for random interpolation
+
+% Define noise level (measured in standard deviations)
+noiseamp = 5; 
+
+% Define amplitude modulator and noise level
+ampl   = interp1(rand(p,1)*30,linspace(1,p,n));
+noise  = noiseamp * randn(size(time));
+signal = ampl + noise;
+
+% Apply a moving average filter
+%   settings
+window = 20;
+plotting = 1;
+%   function
+filtered_signal = moving_average(signal, sampling_rate, window, plotting);
---
+++ denoising/moving_average.m
@@ -0,0 +1,52 @@
+function filtered_signal = moving_average(signal, sampling_rate, window, plotting)
+%% DESCRIPTION
+%
+%   Applies a moving average filter to a time series.
+%
+%   Input:
+%       signal: nx1 array corresponding to the tested time series
+%       sampling_rate: corresponding sampling rate of the time series (i.e.
+%           how many frames per seconds, in Hz)
+%       window: number of frames used to define the size of the window
+%           (e.g. a value of 20 would mean that every data point in the
+%           original signal will be replaced with the mean of the 20 data
+%           points and 20 data points after itself)
+%       plot: set to 1 if you wish to see the resulting filtered signal
+
+%% FUNCTION
+
+% Define time based on signal length and sampling rate
+time = 0:1/sampling_rate:(length(signal)-1)/sampling_rate;
+
+% Define number of frames
+n = length(time);
+
+% Initialize filtered signal vector
+filtered_signal = zeros(size(signal));
+
+% Apply moving average filter with selected window
+for i=window+1:n-window-1
+    % each point is the average of k surrounding points
+    filtered_signal(i) = mean(signal(i-window:i+window));
+end
+
+if plotting == 1
+    % compute window size in ms
+    window_size = 1000*(window*2+1) / sampling_rate;
+
+    % plot the noisy and filtered signals
+    figure(1), clf, hold on
+    plot(time, signal)
+    plot(time, filtered_signal, 'linew',2)
+
+    % draw a patch to indicate the window size
+    tidx = dsearchn(time', 1);
+    ylim = get(gca, 'ylim');
+    patch(time([tidx-window tidx-window tidx+window tidx+window]), ylim([1 2 2 1]), 'k', 'facealpha', .25, 'linestyle','none')
+    plot(time([tidx tidx]), ylim,'k--')
+
+    xlabel('Time (sec.)'), ylabel('Amplitude')
+    title([ 'Moving average filter with a window size of ' num2str(round(window_size)) '-ms filter' ])
+    legend({'Signal'; 'Filtered'; 'Window'; 'window center'})
+else
+end
